81d9739c4a80a3260bc78b16828cec09b4bfe707
Banner-bala <68660070+Banner-bala@users.noreply.github.com>
2020-08-26 23:44:42 +0800

    项目中添加了 vant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入通用样式
+import './style/base.less'
+import './style/iconfont.css'
+
+// 全局导入 vant插件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
